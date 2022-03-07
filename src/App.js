84d542708802c33d6a8f5843ec28ba898a90d62c
mattausloos <ausloosmatt@gmail.com>
2022-03-07 12:08:41 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <img
+          src={require("./images/sky.png")}
+          alt="hi-4"
+          className="background-4"
+        />
+        <img
+          src={require("./images/mountain.png")}
+          alt="hi-3"
+          className="background-3"
+        />
+        <img
+          src={require("./images/houseonhill.png")}
+          alt="hi-2"
+          className="background-2"
+        />
+        <img
+          src={require("./images/bushes.png")}
+          alt="hi-1"
+          className="background-1"
+        />
+
+        <h1>Welcome!</h1>
       </header>
+      <section>
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre kjnkn vjknfekvc fe vf vh rhfv rev
+        rev rcewc ecjrevre v rev rvnrekv revbcfucrk c recrencnerv rehv re vre
+        kjnkn vjknfekvc fe vf vh rhfv rev rev rcewc ecjrevre v rev rvnrekv
+        revbcfucrk c recrencnerv rehv re vre
+      </section>
     </div>
   );
 }
